a0e42bc328c52f994695cc55bfa31a387d3de069
ruikanwang <16578265@qq.com>
2026-03-13 22:11:57 +0800

    1212

--- app/api/faqs/route.ts
+++ app/api/faqs/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server';
 import { faqsStore } from '@/lib/store';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
-  const faqs = faqsStore.getAll();
+  const faqs = await faqsStore.getAll();
   return NextResponse.json(faqs);
 }
